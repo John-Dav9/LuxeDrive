de348b1815e6afe0a80f78e368680c67a74300e9
John David Tchomgui <tchomguijohn@gmail.com>
2024-11-19 14:23:18 +0100

    models generated

---
+++ db/migrate/20241119105748_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.2]
+  def change
+    create_table :bookings do |t|
+      t.date :checkin_date
+      t.date :checkout_date
+      t.boolean :status
+      t.references :offer, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
