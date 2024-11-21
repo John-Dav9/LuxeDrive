36a719faab3a186e5e2aef3c98990692eef115d8
John David Tchomgui <tchomguijohn@gmail.com>
2024-11-21 10:15:23 +0100

    initialize view files

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,12 +3,14 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :destroy]
   before_action :set_car, only: [:new, :create]
 
-  # GET /cars/:car_id/bookings/new
+  def index
+    @bookings = current_user.bookings
+  end
+
   def new
     @booking = Booking.new
   end
 
-  # POST /cars/:car_id/bookings
   def create
     @booking = current_user.bookings.build(booking_params)
     @booking.car = @car
@@ -20,19 +22,11 @@ class BookingsController < ApplicationController
     end
   end
 
-  # GET /bookings
-  def index
-    @bookings = current_user.bookings # Réservations du locataire connecté
-  end
-
-  # GET /bookings/:id
   def show
-    # La réservation est déjà définie par le `before_action :set_booking`
   end
 
-  # DELETE /bookings/:id
   def destroy
-    if @booking.user == current_user # Vérifie que l'utilisateur connecté est bien le locataire
+    if @booking.user == current_user 
       @booking.destroy
       redirect_to bookings_path, notice: 'Réservation annulée avec succès.'
     else
@@ -42,17 +36,14 @@ class BookingsController < ApplicationController
 
   private
 
-  # Définit la réservation à partir des paramètres :id
   def set_booking
     @booking = Booking.find(params[:id])
   end
 
-  # Définit la voiture à partir des paramètres :car_id
   def set_car
     @car = Car.find(params[:car_id])
   end
 
-  # Strong parameters pour les réservations
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,29 +1,24 @@
 class CarsController < ApplicationController
-  before_action :authenticate_user! # S'assurer que l'utilisateur est connecté
+  before_action :authenticate_user!
   before_action :set_car, only: %i[show edit update destroy]
   before_action :authorize_owner, only: %i[edit update destroy]
 
-  # GET /cars
+
   def index
-    # Liste toutes les voitures disponibles
+
     @cars = Car.all
   end
-
-  # GET /cars/:id
   def show
-    # @car est défini dans le `before_action :set_car`
+
   end
 
-  # GET /cars/new
   def new
-    # Prépare une nouvelle voiture pour le formulaire
     @car = Car.new
   end
 
-  # POST /cars
   def create
-    # Crée une nouvelle voiture associée au propriétaire (utilisateur connecté)
-    @car = current_user.cars.build(car_params)
+    @car = Car.new(car_params)
+    @car.user = current_user
     if @car.save
       redirect_to @car, notice: 'Voiture ajoutée avec succès.'
     else
@@ -31,12 +26,9 @@ class CarsController < ApplicationController
     end
   end
 
-  # GET /cars/:id/edit
   def edit
-    # @car est défini dans le `before_action :set_car`
   end
 
-  # PATCH/PUT /cars/:id
   def update
     if @car.update(car_params)
       redirect_to @car, notice: 'Voiture mise à jour avec succès.'
@@ -45,7 +37,6 @@ class CarsController < ApplicationController
     end
   end
 
-  # DELETE /cars/:id
   def destroy
     @car.destroy
     redirect_to cars_path, notice: 'Voiture supprimée avec succès.'
@@ -53,20 +44,17 @@ class CarsController < ApplicationController
 
   private
 
-  # Définit la voiture actuelle à partir des paramètres :id
   def set_car
     @car = Car.find(params[:id])
   end
 
-  # Vérifie si l'utilisateur connecté est le propriétaire de la voiture
   def authorize_owner
     return if @car.user == current_user
 
     redirect_to cars_path, alert: "Vous n'avez pas la permission de modifier ou supprimer cette voiture."
   end
 
-  # Strong parameters pour la création/mise à jour des voitures
   def car_params
-    params.require(:car).permit(:category, :brand, :model, :year, :adress, :price, :options, :status, photos: [])
+    params.require(:car).permit(:category, :brand, :model, :year, :adress, :price, :status, photos: [])
   end
 end
