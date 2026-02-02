ca5e74b564ddb665decbbd63503718f049dbd1e2
John David Tchomgui <tchomguijohn@gmail.com>
2026-02-02 02:23:08 +0100

    Fix PWA manifest/service worker routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # PWA files
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker.js" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest.json" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Super Admin
   namespace :super_admin do
